c68d29dba54309a4a3a5a4f7e0ade52721f1b340
taewan <twk5196@gmail.com>
2021-04-21 20:28:14 +0900

    react map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+
+function Food({fav}){
+  return <h1>i like {fav}</h1>;
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>hello</h1>
+      <Food fav="gimchi"/>
     </div>
+
   );
 }
 
